17184977b2ca881e61ca02aa7c06a371e4346872
mdrayhan03 <phylosopherhossain2022@gmail.com>
2024-06-15 21:04:48 +0600

    commit

---
+++ assets/script/scripts.js
@@ -0,0 +1,4 @@
+function nav_bar() {
+    document.getElementsByClassName("nav-container")[0].style.width = '0px' ;
+    document.getElementsByClassName("menubar-icon")[0].style.display = 'flex' ;
+}
