42c1a6b9fca0a8cd06689841a1e726288afa238f
mdrayhan03 <phylosopherhossain2022@gmail.com>
2024-06-16 17:11:50 +0600

    commit

--- assets/script/scripts.js
+++ assets/script/scripts.js
@@ -1,4 +1,22 @@
-function nav_bar() {
-    document.getElementsByClassName("nav-container")[0].style.width = '0px' ;
-    document.getElementsByClassName("menubar-icon")[0].style.display = 'flex' ;
+var click = false;
+function toggleSidebar() {
+  if (click == false) {
+    // document.getElementsByClassName('nav-profile')[0].style.marginTop = '100px' ;
+    document.getElementsByClassName("profile-info")[0].style.display = "none";
+    var elementArray = document.getElementsByClassName("btn-text");
+    for (var i = 0; i < elementArray.length; ++i) {
+      document.getElementsByClassName("btn-text")[i].style.display = "none";
+    }
+    document.getElementsByClassName("nav-container")[0].style.width = "50px";
+    click = true;
+  } else {
+    // document.getElementsByClassName('nav-profile')[0].style.marginTop = '50px' ;
+    document.getElementsByClassName("profile-info")[0].style.display = "block";
+    document.getElementsByClassName("nav-container")[0].style.width = "300px";
+    var elementArray = document.getElementsByClassName("btn-text");
+    for (var i = 0; i < elementArray.length; ++i) {
+      document.getElementsByClassName("btn-text")[i].style.display = "inline";
+    }
+    click = false;
+  }
 }
